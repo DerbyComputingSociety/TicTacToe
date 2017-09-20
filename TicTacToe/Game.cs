0710376babebfc2ee7bc693473fbd78d1b2d31dd
Cat Flynn <mewtwo97@hotmail.com>
2017-09-20 22:15:39 +0100

    Remove static references

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+public class Game {
+	char[] _board = {
+        ' ',
+    	'1', '2', '3',
+    	'4', '5', '6',
+    	'7', '8', '9'
+    };
+	char[] _playerSymbols = { 'X', 'O' };
+    int _currentPlayer;
+
+    public void Run() {
+        int flag = 0;
+
+        while (flag == 0) {
+			Console.Clear();
+			
+			Console.WriteLine("Player1:X and Player2:O");
+			Console.WriteLine();
+			if (_currentPlayer == 0) {
+				Console.WriteLine("Player 1 Chance");
+			} else {
+				Console.WriteLine("Player 2 Chance");
+			}
+			Console.WriteLine();
+			DrawBoard(_board);
+			var choice = int.Parse(Console.ReadLine());
+			
+			while (choice > 9 || choice < 0) {
+				Console.WriteLine("That is an invalid number, please try again: ");
+				choice = int.Parse(Console.ReadLine());
+			}
+			
+			if (_board[choice] != 'X' && _board[choice] != 'O') {
+				_board[choice] = _playerSymbols[_currentPlayer];
+			} else {
+				Console.WriteLine("Sorry the row {0} is already marked with {1}", choice, _board[choice]);
+				Console.WriteLine();
+			}
+			flag = CheckGameOver(_board);
+            if (flag != 0)
+                break;
+            
+            _currentPlayer = _currentPlayer == 0 ? 1 : 0;
+        }
+
+		Console.Clear();
+		DrawBoard(_board);
+		if (flag == 1) {
+			Console.WriteLine("Player {0} has won", _currentPlayer + 1);
+		} else {
+			Console.WriteLine("Draw");
+		}
+		Console.ReadLine();
+	}
+
+	void DrawBoard(char[] board) {
+		Console.WriteLine("   |   |  ");
+		Console.WriteLine(" {0} | {1} | {2}", board[1], board[2], board[3]);
+		Console.WriteLine("___|___|___");
+		Console.WriteLine("   |   |  ");
+		Console.WriteLine(" {0} | {1} | {2}", board[4], board[5], board[6]);
+		Console.WriteLine("___|___|___");
+		Console.WriteLine("   |   |  ");
+		Console.WriteLine(" {0} | {1} | {2}", board[7], board[8], board[9]);
+		Console.WriteLine("   |   |  ");
+	}
+
+	private int CheckGameOver(char[] board) {
+		// Check rows
+		if ((board[1] == board[2] && board[2] == board[3]) ||
+			(board[4] == board[5] && board[5] == board[6]) ||
+			(board[7] == board[8] && board[8] == board[9]))
+			return 1;
+
+		// Check columns
+		if ((board[1] == board[4] && board[4] == board[7]) ||
+		   (board[2] == board[5] && board[5] == board[8]) ||
+		   (board[3] == board[6] && board[6] == board[9]))
+			return 1;
+
+		// Check Diagonals
+		if ((board[1] == board[5] && board[5] == board[9]) ||
+			(board[3] == board[5] && board[5] == board[7]))
+			return 1;
+
+		// Check For Draw
+		if (board[1] != '1' && board[2] != '2' && board[3] != '3' && board[4] != '4' && board[5] != '5' && board[6] != '6' && board[7] != '7' && board[8] != '8' && board[9] != '9')
+			return -1;
+
+		return 0;
+	}
+}
+
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,116 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class Program
-{
-    static int player = 1;
-    static int choice;
-    static int flag = 0;
-
-    private static int CheckGameOver(char[] board)
-    {
-        // Check rows
-        if ((board[1] == board[2] && board[2] == board[3]) ||
-            (board[4] == board[5] && board[5] == board[6]) ||
-            (board[7] == board[8] && board[8] == board[9]))
-            return 1;
-        
-        // Check columns
-        if ((board[1] == board[4] && board[4] == board[7]) ||
-           (board[2] == board[5] && board[5] == board[8]) || 
-           (board[3] == board[6] && board[6] == board[9]))
-            return 1;
-        
-       // Check Diagonals
-        if ((board[1] == board[5] && board[5] == board[9]) ||
-            (board[3] == board[5] && board[5] == board[7]))
-            return 1;
-        
-        // Check For Draw
-        if (board[1] != '1' && board[2] != '2' && board[3] != '3' && board[4] != '4' && board[5] != '5' && board[6] != '6' && board[7] != '7' && board[8] != '8' && board[9] != '9')
-            return -1;
-        
-        return 0;
-    }
-    private static void DrawBoard(char[] board)
-    {
-        Console.WriteLine("   |   |  ");
-        Console.WriteLine(" {0} | {1} | {2}", board[1], board[2], board[3]);
-        Console.WriteLine("___|___|___");
-        Console.WriteLine("   |   |  ");
-        Console.WriteLine(" {0} | {1} | {2}", board[4], board[5], board[6]);
-        Console.WriteLine("___|___|___");
-        Console.WriteLine("   |   |  ");
-        Console.WriteLine(" {0} | {1} | {2}", board[7], board[8], board[9]);
-        Console.WriteLine("   |   |  ");
-    }
-    static void Main(string[] args)
-    {
-        var board = new char[] { 
-            '0', 
-            '1', '2', '3', 
-            '4', '5', '6', 
-            '7', '8', '9'
-        };
-
-        do
-        {
-            Console.Clear();
-            Console.WriteLine("Player1:X and Player2:O");
-            Console.WriteLine();
-            if (player % 2 == 0)
-            {
-                Console.WriteLine("Player 2 Chance");
-            }
-            else
-            {
-                Console.WriteLine("Player 1 Chance");
-            }
-            Console.WriteLine();
-            DrawBoard(board);
-            choice = int.Parse(Console.ReadLine());
-
-            while (choice > 9 || choice < 0)
-            {
-                Console.WriteLine("That is an invalid number, please try again: ");
-                choice = int.Parse(Console.ReadLine());
-            }
-
-            if (board[choice] != 'X' && board[choice] != 'O')
-            {
-                if (player % 2 == 0)
-				{
-                    board[choice] = '0';
-                    player++;
-                }
-                else
-                {
-                    board[choice] = 'X';
-                    player++;
-                }
-
-            }
-            else
-            {
-                Console.WriteLine("Sorry the row {0} is already marked with {1}", choice, board[choice]);
-                Console.WriteLine();
-
-            }
-            flag = CheckGameOver(board);
+namespace TicTacToe {
+    class Program {
+        static void Main(string[] args) {
+            var game = new Game();
+            game.Run();
         }
-        while (flag != 1 && flag != -1);
-
-        Console.Clear();
-        DrawBoard(board);
-        if (flag == 1)
-        {
-            Console.WriteLine("Player {0} has won", (player % 2) + 1);
-        }
-        else
-        {
-            Console.WriteLine("Draw");
-        }
-        Console.ReadLine();
-
     }
 }
+
